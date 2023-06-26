f4321ba087120bdf2ecb6f8969ea12932d51a650
jerrygmail <jerrymanaloto@gmail.com>
2023-06-26 12:00:20 +0800

    Adding existing project files to git

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,18 @@
+//package io.happycoding.servlets;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.IOException;
+/*import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+*/
+//@WebServlet("/hello")
+public class HelloWorld extends HttpServlet {
+@Override
+public void doGet(HttpServletRequest request, HttpServletResponse response)
+throws IOException {
+response.setContentType("text/html;");
+response.getWriter().println("<h1>Hello world2!</h1>");
+}
+}
